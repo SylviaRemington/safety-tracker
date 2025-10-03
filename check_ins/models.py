e7b22e49f00c1c67cb732ee911b4c275a8924d9f
SylviaRemington <sylviaremington@gmail.com>
2025-10-03 00:26:37 -0700

    updating models.py & making migrations & installing rest framework

--- check_ins/models.py
+++ check_ins/models.py
@@ -6,9 +6,9 @@ class Check_In(models.Model):
     return f'{self.title} - {self.category}'
   title = models.CharField(max_length=80, unique=True)
   description = models.CharField(max_length=5000)
-  category = models.CharField(max_length=60)
+  category = models.CharField(max_length=80)
   reaction_level = models.FloatField()
-  coping_action = models.CharField(max_length=60)
+  coping_action = models.CharField(max_length=5000)
   effectiveness = models.FloatField()
 
 # Alternate Check_In Model I could use as per researching (for future when I want to make it more accurate; however, maybe not use blank=True):
