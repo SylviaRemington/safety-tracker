791bcdcad5a0f896f319f8d8ac8efb43256f2313
SylviaRemington <sylviaremington@gmail.com>
2025-10-03 00:03:45 -0700

    registering the model in admin.py so that the admin site can pick it up

--- check_ins/admin.py
+++ check_ins/admin.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 # Registering my models here.
 # This is where I'm importing the Check_In
 from .models import Check_In
+
+# Registering the model here so the admin site can pick it up
+admin.site.register(Check_In)
